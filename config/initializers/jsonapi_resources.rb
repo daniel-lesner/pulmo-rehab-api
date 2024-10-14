deea2e73a5f3bfa790e9f2605e26d72e9b401e10
Daniel Lesner <daniel.lesner@icloud.com>
2024-10-14 17:27:48 +0200

    Configure jsonapi-resources and rubocop.yml file

---
+++ config/initializers/jsonapi_resources.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+JSONAPI.configure do |config|
+  # Built-in key format options are :underscored_key, :camelized_key, and :dasherized_key
+  config.json_key_format = :camelized_key
+end
