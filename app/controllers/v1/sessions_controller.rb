4e98030bee6d74a6547ca2425186399ec5365e4c
Daniel Lesner <daniel.lesner@icloud.com>
2024-12-03 23:39:30 +0100

    Workaround to invalidate wrong email/password user connection in controller

--- app/controllers/v1/sessions_controller.rb
+++ app/controllers/v1/sessions_controller.rb
@@ -3,5 +3,25 @@
 module V1
   class SessionsController < ApplicationController
     skip_before_action :authenticate_user, only: [ :create ]
+
+    def create
+      email = params["data"]["attributes"]["email"]
+      password = params["data"]["attributes"]["password"]
+
+      if V1::User.find_by("email": email, password: password).nil?
+        return render json: {
+          errors: [
+              {
+                  "title": "Invalid details",
+                  "detail": "password does not match email",
+                  "code": "422",
+                  "status": "422"
+              }
+          ]
+        }, status: 422
+      end
+
+      super
+    end
   end
 end
