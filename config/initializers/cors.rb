9187745448ef8f0e754dcc6ade54e08bc5c6acde
Daniel Lesner <daniel.lesner@icloud.com>
2024-12-13 20:52:54 +0100

    Remove trailing slash from cors origins url

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://pulmo-rehab.com/"
+    origins "https://pulmo-rehab.com"
 
     resource "*",
       headers: :any,
