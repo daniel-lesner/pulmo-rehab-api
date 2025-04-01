6e7dc147523f54569e4f34879a9a33060782e6b2
Daniel Lesner <daniel.lesner@icloud.com>
2025-04-02 00:36:56 +0300

    Allow camelized routes

--- config/initializers/jsonapi_resources.rb
+++ config/initializers/jsonapi_resources.rb
@@ -3,4 +3,5 @@
 JSONAPI.configure do |config|
   # Built-in key format options are :underscored_key, :camelized_key, and :dasherized_key
   config.json_key_format = :camelized_key
+  config.route_format = :camelized_route
 end
